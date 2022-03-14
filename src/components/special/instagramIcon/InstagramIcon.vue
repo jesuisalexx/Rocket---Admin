bcb416b203b9aabb52620ec99ce47eac24777fda
Ruslan Lei <rusya.lay@gmail.com>
2022-03-14 22:44:11 +0400

    chore: fix and add special components

---
+++ src/components/special/instagramIcon/InstagramIcon.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="instagram-camera__wrapper">
+    <div
+      :class="{
+        'instagram-camera': true,
+        'instagram-camera--active': isActive
+      }"
+    >
+      <div class="instagram-camera__backside" />
+      <div class="instagram-camera__body">
+        <div class="instagram-camera__flash" />
+      </div>
+      <div class="instagram-camera__lens" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { PropType } from 'vue';
+
+defineProps({
+  isActive: {
+    type: Boolean as PropType<boolean>,
+    default: false,
+  },
+});
+</script>
+
+<style lang="scss" src="./instagramIcon.scss" />
---
+++ src/components/special/telegramIcon/TelegramIcon.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="telegram-airplane__wrapper">
+    <div
+      class="telegram-airplane"
+    >
+      <div class="telegram-airplane__wings">
+        <div class="telegram-airplane__wing-left">
+          <TelegramAirplaneWings class="telegram-airplane__wing-left-svg" />
+        </div>
+        <div class="telegram-airplane__wing-right">
+          <TelegramAirplaneWings class="telegram-airplane__wing-right-svg" />
+        </div>
+      </div>
+      <div class="telegram-airplane__bottom">
+        <div class="telegram-airplane__bottom-part-left">
+          <TelegramAirplaneBottomPart class="telegram-airplane__bottom-part-left-icon" />
+        </div>
+        <div class="telegram-airplane__bottom-part-right">
+          <TelegramAirplaneBottomPart class="telegram-airplane__bottom-part-right-icon" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import TelegramAirplaneWings from './assets/telegramAirplaneWings.svg';
+import TelegramAirplaneBottomPart from './assets/telegramAirplaneBottomPart.svg';
+</script>
+
+<style lang="scss" src="./telegramIcon.scss" />

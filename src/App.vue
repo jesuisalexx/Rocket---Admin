9fd62641f9e56fa265fb19fc11c9dc70b42ccf0e
Ruslan Lei <rusya.lay@gmail.com>
2022-03-15 10:22:58 +0400

    fix: move layout logic

--- src/App.vue
+++ src/App.vue
@@ -5,39 +5,7 @@
 </template>
 
 <script setup lang="ts">
-import {
-  ref,
-  watch,
-  onBeforeUnmount,
-  onMounted,
-} from 'vue';
-import { useCommonStore } from '@/stores/common';
-import { theme } from '@/types/theme';
 import Layout from '@/components/core/layout/Layout.vue';
-
-const commonStore = useCommonStore();
-
-const activeTheme = ref(theme.LIGHT);
-watch(activeTheme, (value) => {
-  commonStore.setTheme(value);
-});
-
-const handleBaseUnit = () => {
-  const vh = window.innerHeight / 100;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-  commonStore.setVh(vh);
-};
-
-onMounted(() => {
-  handleBaseUnit();
-  window.addEventListener('resize', handleBaseUnit);
-  window.addEventListener('orientationchange', handleBaseUnit);
-});
-
-onBeforeUnmount(() => {
-  window.removeEventListener('resize', handleBaseUnit);
-  window.removeEventListener('orientationchange', handleBaseUnit);
-});
 </script>
 
 <style lang="scss">
--- src/components/core/layout/Layout.vue
+++ src/components/core/layout/Layout.vue
@@ -8,12 +8,14 @@
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue';
+import { computed, onBeforeUnmount, onMounted } from 'vue';
 import { useRoute } from 'vue-router';
-
 import defaultLayout from '@/layouts/default/Default.vue';
+import { useCommonStore } from '@/stores/common';
 
 const route = useRoute();
+
+const commonStore = useCommonStore();
 
 const layoutsMap: Record<string, any> = {
   default: defaultLayout,
@@ -24,6 +26,23 @@ const currentLayout = computed(() => {
     ? String(route.meta.layout)
     : 'default';
   return layoutsMap[layoutName];
+});
+
+const handleBaseUnit = () => {
+  const vh = window.innerHeight / 100;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+  commonStore.setVh(vh);
+};
+
+onMounted(() => {
+  handleBaseUnit();
+  window.addEventListener('resize', handleBaseUnit);
+  window.addEventListener('orientationchange', handleBaseUnit);
+});
+
+onBeforeUnmount(() => {
+  window.removeEventListener('resize', handleBaseUnit);
+  window.removeEventListener('orientationchange', handleBaseUnit);
 });
 </script>
 
